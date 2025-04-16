6d29be2b45465684b5511826b8562a73f91418f2
Guilherme Athayde <guiathayde303@gmail.com>
2025-04-16 15:04:17 -0300

    add start-all script

--- scripts/restart-all.bash
+++ scripts/restart-all.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$SCRIPT_DIR/.."
 
+cd "$SCRIPT_DIR/.."
 docker stop api-gateway
 docker rm api-gateway
 docker build -t api-gateway .
--- scripts/start-all.bash
+++ scripts/start-all.bash
@@ -4,3 +4,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "$SCRIPT_DIR/.."
 docker build -t api-gateway .
+docker run -d -p 443:443 --name api-gateway --add-host host.docker.internal:host-gateway api-gateway
+docker logs -f api-gateway
